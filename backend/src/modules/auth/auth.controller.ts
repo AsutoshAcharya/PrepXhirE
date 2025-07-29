1d8ab57eadd709899ab09f02f63ddd1e4719bc9b
Asutosh Acharya <asutosh246@gmail.com>
2025-07-29 10:23:25 +0530

    minor fix related to this

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -4,17 +4,22 @@ import { Dependencies } from "../../container";
 import AuthService from "./auth.service";
 
 export default class AuthController {
-  private readonly authService;
+  private readonly authService: AuthService;
 
   constructor({ authService }: Dependencies) {
     this.authService = authService;
+
+    //we can use bind or direct arrow function
+
+    // this.logIn = this.logIn.bind(this);
+    // this.register = this.register.bind(this);
   }
 
-  public register(req: Request, res: Response) {
+  public register = (req: Request, res: Response) => {
     this.authService.register();
-  }
+  };
 
-  public logIn(req: Request, res: Response) {}
+  public logIn = (req: Request, res: Response) => {};
 
   // public isMe(req: Request, res: Response): Promise<void> {
 
