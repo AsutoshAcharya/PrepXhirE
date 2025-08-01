5d3d4779dc54af2f6525b1e8f05544a4d083b1d9
Asutosh Acharya <asutosh246@gmail.com>
2025-08-01 16:02:54 +0530

    updated auth schema

--- backend/src/modules/auth/auth.schema.ts
+++ backend/src/modules/auth/auth.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { JobTitle, UserRole } from "../../models/user.model";
 
 export const registerSchema = z.object({
-  fullName: z.string().min(1),
+  fullName: z.string().min(1).max(40),
   email: z.string().max(65),
   password: z.string().min(6),
   role: z.enum(UserRole).optional(),
@@ -17,4 +17,4 @@ export const loginSchema = registerSchema.pick({
 });
 
 export type RegisterDto = z.infer<typeof registerSchema>;
-export type LoginDto = Pick<RegisterDto, "email" | "password">;
+export type LoginDto = z.infer<typeof loginSchema>;
